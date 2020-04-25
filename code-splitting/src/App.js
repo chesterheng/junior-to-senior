dce3d9fd0b9a047f6a02a81861a1eccc5cdd7ace
Chester Heng <chester.heng@gmail.com>
2020-04-26 06:16:37 +0800

    05-111:Code Splitting - manual

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Page1 from './Components/Page1';
 // Part 1 - No Code Splitting
-import Page2 from './Components/Page2';
-import Page3 from './Components/Page3';
+// import Page2 from './Components/Page2';
+// import Page3 from './Components/Page3';
 // Part 3 - Cleaner Code Splitting
 // import AsyncComponent from './AsyncComponent';
 
@@ -14,48 +14,48 @@ class App extends Component {
     this.state = {
       route: 'page1',
       // Part 2 - Code Splitting - manual
-      // component: null
+      component: null
     }
   }
   onRouteChange = (route) => {
     // Part 1 - No Code Splitting
-    this.setState({ route: route });
+    // this.setState({ route: route });
     // Part 2 - Code Splitting - manual
-    // if (route === 'page1') {
-    //   this.setState({ route: route })
-    // } else if (route === 'page2') {
-    //   import('./Components/Page2')
-    //     .then((Page2) => {
-    //       this.setState({ route: route, component: Page2.default })
-    //     })
-    //     .catch(err => {
-    //     });
-    // } else {
-    //   import('./Components/Page3')
-    //     .then((Page3) => {
-    //       this.setState({ route: route, component: Page3.default })
-    //     })
-    //     .catch(err => {
-    //       console.log(err)
-    //     });
-    // }
+    if (route === 'page1') {
+      this.setState({ route: route })
+    } else if (route === 'page2') {
+      import('./Components/Page2')
+        .then((Page2) => {
+          this.setState({ route: route, component: Page2.default })
+        })
+        .catch(err => {
+        });
+    } else {
+      import('./Components/Page3')
+        .then((Page3) => {
+          this.setState({ route: route, component: Page3.default })
+        })
+        .catch(err => {
+          console.log(err)
+        });
+    }
   }
   render() {
     // Part 1 - No code splitting
-    if (this.state.route === 'page1') {
-      return <Page1 onRouteChange={this.onRouteChange} />
-    } else if (this.state.route === 'page2') {
-      return <Page2 onRouteChange={this.onRouteChange} />
-    } else {
-      return <Page3 onRouteChange={this.onRouteChange} />
-    }
-
-    // Part 2 - No Code Splitting - manual
     // if (this.state.route === 'page1') {
     //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   return <Page2 onRouteChange={this.onRouteChange} />
     // } else {
-    //   return <this.state.component onRouteChange={this.onRouteChange} />
+    //   return <Page3 onRouteChange={this.onRouteChange} />
     // }
+
+    // Part 2 - No Code Splitting - manual
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
 
     // Part 3 - Cleaner Code Splitting
     // if (this.state.route === 'page1') {
