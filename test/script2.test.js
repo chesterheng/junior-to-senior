ba5e3c8a27dd3f8540f364512bb57ae89304e435
Chester Heng <chester.heng@gmail.com>
2020-04-27 05:57:24 +0800

    06-156:Mocks and Spies

--- test/script2.test.js
+++ test/script2.test.js
@@ -11,7 +11,13 @@ describe('swapi', () => {
   // })
 
   it('calls swapi to get people', () => {
+    //expect.assertions(number) verifies that a
+    // certain number of assertions are called during a
+    // test. This is often useful when testing asynchronous
+    //code, in order to make sure that assertions in a
+    //callback actually got called.
     expect.assertions(1)
+    // return promise so expect.assertions(1) can detect
     return swapi.getPeople(fetch).then(data => {
       expect(data.count).toEqual(82)
     })
@@ -24,5 +30,25 @@ describe('swapi', () => {
       expect(data.results.length).toBeGreaterThan(5)
     })
   })
+
+  it('getPeople returns count and results', () => {
+    const mockFetch = jest.fn()
+      .mockReturnValue(Promise.resolve({
+        json: () => Promise.resolve({
+          count: 87,
+          results: [0,1,2,3,4,5]
+        })
+      }))
+    
+    expect.assertions(4)
+    // use dependency injection to inject mockFetch instead of fetch
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+      expect(mockFetch.mock.calls.length).toBe(1)
+      expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people')
+      expect(data.count).toEqual(87)
+      expect(data.results.length).toBeGreaterThan(5)
+    })
+  })
+
 })
 
