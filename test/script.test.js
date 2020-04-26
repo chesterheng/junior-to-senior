aefd1f062751ac27ef7f11a457b57c6880ec207a
Chester Heng <chester.heng@gmail.com>
2020-04-27 04:45:48 +0800

    06-151:Writing Tests

--- test/script.test.js
+++ test/script.test.js
@@ -7,11 +7,23 @@ dbMock = [
   'dogpictures.com'
 ]
 
-it('this is a silly test', () => {
-  expect('hello').toBe('hello');
+describe('googleSearch', () => {
+  it('this is a silly test', () => {
+    expect('hello').toBe('hello');
+  })
+  
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  })
+  
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  })
+  
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  })
 })
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
-})
