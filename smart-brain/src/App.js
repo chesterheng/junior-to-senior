5e0bef7c652bf0c53a98b09ef6d6260602931b73
Chester Heng <chester.heng@gmail.com>
2020-05-05 07:07:07 +0800

    13-311:Profile Modal Component

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Modal from './components/Modal/Modal'
 import './App.css';
 
 const particlesOptions = {
---
+++ smart-brain/src/components/Modal/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Modal
