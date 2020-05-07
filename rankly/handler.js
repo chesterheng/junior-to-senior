b81956e9fc58642b9719c2b7f30858df7a00020b
Chester Heng <chester.heng@gmail.com>
2020-05-08 06:15:39 +0800

    14-349:Deploying A Function - implement rank function

--- rankly/handler.js
+++ rankly/handler.js
@@ -1,12 +1,18 @@
 'use strict';
 
+const emojis = [
+  '😄','😃','😀','😊','😉','😍','🔶','🔷', '🚀'
+];
+
 module.exports.rank = async event => {
+  // refer to API endpoint for queryStringParameters
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+    body: JSON.stringify({
+        message: 'Rank generated!',
+        input: rankEmoji,
       },
       null,
       2
